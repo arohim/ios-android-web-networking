9763a5fb94dd3ffb85a4d8b2e26e27d24c6915aa
Abdulrohim 'Him' Sama <abdulrohim.s@kaidee.com>
2019-09-03 17:50:49 +0700

    call api from android

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jetbrains.handson.mpp.mobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
+
+        button_call_api.setOnClickListener {
+            val weatherApi = WeatherApi()
+            val repo = WeatherRepositoryImpl(weatherApi)
+            repo.sendTracking("data") {
+                println("result $it")
+            }
+        }
     }
 }
